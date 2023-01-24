b447563bbc0c6679d3f27192d90d50d05004934b
Azfar41268 <azfarsikander33@gmail.com>
2023-01-24 22:20:36 +0500

    ok

--- app/price.tsx
+++ app/price.tsx
@@ -4,7 +4,7 @@ import {Icons} from "./icon"
 export const ListItem= (props: StackProps) => {
     const {children, ...rest} = props
     return(
-        <HStack as="li" spacing="1.25rem" {...rest}>
+        <HStack as="li" spacing="1.25rem" {...rest} alignItems="start">
             <Icon as={Icons} w="1.375rem" h="1.375rem" />
             <Text textAlign={["left", "left", "center"]}>{children}</Text>
         </HStack>
@@ -14,13 +14,13 @@ export const ListItem= (props: StackProps) => {
 export default function Pricing() {
     return(
         <Box mx="24px">
-            <Box as="section" maxW="62.125rem" margin="auto" mt="-15.625rem" borderRadius="0.75rem" overflow="hidden" boxShadow="0px 20px 25px -5px rgba(0, 0, 0, 0.1), 0px 10px 10px -5px rgba(0, 0, 0, 0.04)">
-                <Flex bg="white" direction={["column", "column", "row"]} textAlign="center">
-                    <Box bg="#F0EAFB" p="3.75rem" textAlign="center">
+            <Box textAlign="center" as="section" maxW="62.125rem" margin="auto" mt="-15.625rem" borderRadius="0.75rem" overflow="hidden" boxShadow="0px 20px 25px -5px rgba(0, 0, 0, 0.1), 0px 10px 10px -5px rgba(0, 0, 0, 0.04)">
+                <Flex bg="white" direction={["column", "column", "row"]}>
+                    <Box bg="#F0EAFB" p="3.75rem">
                         <Text color="black" fontSize="1.5rem" fontWeight="extrabold">
                             Premium PRO
                         </Text>
-                        <Heading as="h3" color="black" mt="1.5rem" fontSize="3.75rem">
+                        <Heading as="h3" color="black" mt="1.5rem" fontSize={["48px", "48px", "60px"]}>
                             $369
                         </Heading>
                         <Text color="black" mt="1.5rem">
@@ -30,8 +30,8 @@ export default function Pricing() {
                             Get Started
                         </Button>
                     </Box>
-                    <Box  alignSelf="center" color="black" fontSize="1.125rem" ml="4.375rem" bg="white">
-                        <Text alignSelf="left">
+                    <Box color="black" fontSize="1.125rem" bg="white" ml="30px" mt={["20px", "20px", "50px"]}>
+                        <Text textAlign="left">
                         Access these features when you get this pricing package for your business.
                         </Text>
                         <Stack as="ul" spacing="1.25rem" pt="1.5rem">
